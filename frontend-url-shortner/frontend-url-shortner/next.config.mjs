84f711ed07cee41dec2135733536194f7eeacaf9
sujalsharmaa <sujalsharma151@gmail.com>
2024-11-15 12:26:00 +0530

    updated dockerfile

--- frontend-url-shortner/frontend-url-shortner/next.config.mjs
+++ frontend-url-shortner/frontend-url-shortner/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+  
+    // Define environment variables to be accessible in the frontend
+    env: {
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+      NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+    },
+  };
+  
+  export default nextConfig;
+  
