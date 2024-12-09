578c62b60be9473396d337b706aee7190e0cfd68
sujalsharmaa <sujalsharma151@gmail.com>
2024-12-09 18:41:35 +0530

    changed the alertnemanager.yaml

--- terraform-for-project-1/provider.tf
+++ terraform-for-project-1/provider.tf
@@ -8,6 +8,7 @@ terraform {
   #   region         = "us-east-1"
   #   key            = "mystate"
   #  // dynamodb_table = "Lock-Files"
+  #   dynamodb_table = "Lock-Files"
   #   encrypt        = true
   # }
   required_version = ">=1.0"
